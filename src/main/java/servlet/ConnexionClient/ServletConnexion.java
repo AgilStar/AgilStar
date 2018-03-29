f35a6594d4c9c8baa07eea010d81a00d5442a652
tianyuanliu <396683431@qq.com>
2018-03-29 10:05:51 +0200

    ss

--- src/main/java/servlet/ConnexionClient/ServletConnexion.java
+++ src/main/java/servlet/ConnexionClient/ServletConnexion.java
@@ -28,13 +28,13 @@ public class ServletConnexion extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-        String email = req.getParameter("email");
-        String mdp = req.getParameter("mdp");
-        //out.print(email);
-        //out.print(mdp);
-        String url = "";
-        url = new db.dbClient().verifyConnect(email, mdp);
-         out.print(url);
-      
+//        String email = req.getParameter("email");
+//        String mdp = req.getParameter("mdp");
+//        //out.print(email);
+//        //out.print(mdp);
+//        String url = "";
+//        url = new db.dbClient().verifyConnect(email, mdp);
+         //out.print(url);
+      out.print("jhkjhj");
     }}
 
